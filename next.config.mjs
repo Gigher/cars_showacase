6a6fba91d6e1aabd668fd56d4ee106e8ed2532a8
Gigher <gastelo1301@gmail.com>
2024-03-02 15:58:05 +0300

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['cdn.imagin.studio']
+    }
+};
 
 export default nextConfig;
+
+// const nextConfig = {
+//     images: {
+//       remotePatterns: [
+//         {
+//             protocol: 'https',
+//             hostname: 'cdn.imagin.studio',
+//             port: '',
+//             pathname: 'utils/index.ts'
+//         },
+//       ],
+//     },
+//   };
